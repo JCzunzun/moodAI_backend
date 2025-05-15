88a3b4cf03b5724575b9ca01fa99f83ef4e3a217
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2025-05-15 10:28:57 +0200

    springinitializr create project

---
+++ src/main/java/com/jczunzun/moodAi/MoodAiApplication.java
@@ -0,0 +1,13 @@
+package com.jczunzun.moodAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jczunzun/moodAi/MoodAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jczunzun.moodAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
